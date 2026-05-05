b9180eb88b34770f8ec275300598432ca2beb5bf
Petr <peterpankratc@gmail.com>
2026-05-05 21:09:06 +0300

    2 задание полностью

--- .venv/2 задание.sql
+++ .venv/2 задание.sql
@@ -15,9 +15,42 @@ CREATE TABLE IF NOT EXISTS author_album (
     PRIMARY KEY (author_id, album_id)
 );
 
-CREATE TABLE songs (
+    
+CREATE TABLE IF NOT EXISTS songs (
     song_id SERIAL PRIMARY KEY,
     album_id INT REFERENCES album(album_id),
     song_time time not null,
     title varchar(30) not null
-)
+);
+
+CREATE TABLE IF NOT EXISTS genre (
+    genre_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS song_genre (
+    song_id INT REFERENCES songs(song_id),
+    genre_id INT REFERENCES genre(genre_id),
+    PRIMARY KEY (song_id, genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS author_genre (
+    author_id INT REFERENCES author(author_id),
+    genre_id INT REFERENCES genre(genre_id),
+     PRIMARY KEY (author_id, genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS collection (
+    collection_id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    release_year date NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS collection_song (
+    song_id INT REFERENCES songs(song_id),
+    collection_id INT REFERENCES collection(collection_id),
+    PRIMARY KEY (song_id, collection_id)
+);
+     
+     
